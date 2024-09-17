82b6431ee1846691c67798c2c9c8bbfcc72557e7
trnguyenuka <trnguyenuka@github.com>
2024-09-17 21:42:24 +0200

    update

--- official/install_clusterProfiler.R
+++ official/install_clusterProfiler.R
@@ -5,5 +5,6 @@
 # remove.packages("GOSemSim")
 # remove.packages("yulab.utils")
 # remove.packages("clusterProfiler")
+devtools::install_github("YuLab-SMU/yulab.utils", upgrade = "never")
 remotes::install_github("GuangchuangYu/GOSemSim", upgrade = "never")
 remotes::install_github("GuangchuangYu/clusterProfiler", upgrade = "never")
