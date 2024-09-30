9bb92ec76d84a5aa89f32661c1a09a6a9ece0626
trnguyenuka <trnguyenuka@github.com>
2024-09-30 20:57:29 +0200

    update

---
+++ official_SeuratV4/install_rmarkdown.R
@@ -0,0 +1,6 @@
+install.packages('/media/hieunguyen/HD01/storage/offline_pkgs/knitr_1.45.tar.gz', 
+                 type = "source",
+                 repos = NULL) 
+install.packages('/media/hieunguyen/HD01/storage/offline_pkgs/rmarkdown_2.26.tar.gz',
+                 type = "source",
+                 repos = NULL)
